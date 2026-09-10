14c66b99e44757e63168eeb7cfbf8504e3328da1
crayon11 <1343931491@qq.com>
2026-09-10 23:10:37 +0800

    add test

--- clone_1.py
+++ clone_1.py
@@ -1,3 +1,13 @@
 print("hello world")
 print("hello world")
 print("hello world")
+
+
+def greet(name: str) -> str:
+    return f"Hello, {name}!"
+
+def main() -> None:
+    print(greet("World"))
+
+if __name__ == "__main__":
+    main()
